db5192ba568449600ced72cd08edd793973a1d67
akitsu-sanae <akitsu.sanae@gmail.com>
2019-12-12 13:15:25 +0900

    add copyright notice

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -4,8 +4,9 @@ import Browser
 import CVC4.Model
 import CVC4.Update
 import CVC4.View
-import Html exposing (Html, a, button, code, div, h1, header, label, li, pre, text, textarea, ul)
-import Html.Attributes exposing (class, for, href, rows)
+import Html exposing (Html, a, button, code, div, h1, h4, h5, header, label, li, p, pre, text, textarea, ul)
+import Html.Attributes exposing (class, for, href, id, rows)
+import Html.Attributes.Aria exposing (ariaLabel, role)
 import Html.Events exposing (onClick, onInput)
 import Http
 import Json.Decode
@@ -262,10 +263,107 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
     div [ class "container" ]
-        [ header [] [ h1 [] [ text "Plover" ] ]
+        [ headerView
         , div [ class "columns" ]
             [ div [ class "column col-4-mr-auto" ] [ paramsView model.params ]
             , div [ class "column col-8" ] [ mainView model ]
+            ]
+        ]
+
+
+copyrightView : Html Msg
+copyrightView =
+    div [ class "content" ]
+        [ h4 [] [ text "Plover Garden" ]
+        , p []
+            [ text
+                "Copyright (C) 2019 akitsu sanae.\n                        Distributed under the Boost Software License, Version 1.0. (See accompanying file LICENSE_1_0.txt)"
+            ]
+        , p [] [ text "Plover Garden depends on ..." ]
+        , h5 [] [ text "Z3" ]
+        , p [] [ text "Copyright (c) Microsoft Corporation" ]
+        , p [] [ text "All rights reserved." ]
+        , p [] [ text "MIT License" ]
+        , p [] [ text "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \" Software \"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:" ]
+        , p [] [ text "The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software." ]
+        , p [] [ text "THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE." ]
+        , h5 [] [ text "CVC4" ]
+        , p [] [ text """CVC4 is copyright (C) 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017 by
+            its authors and contributors (see the file https://github.com/CVC4/CVC4/blob/master/AUTHORS) and their institutional
+            affiliations.  All rights reserved.""" ]
+        , p [] [ text """The source code of CVC4 is open and available to students, researchers,
+            software companies, and everyone else to study, to modify, and to redistribute
+            original or modified versions; distribution is under the terms of the modified
+            BSD license (reproduced below).  Please note that CVC4 can be configured
+            (however, by default it is not) to link against some GPLed libraries, and
+            therefore the use of these builds may be restricted in non-GPL-compatible
+            projects.  See below for a discussion of CLN, GLPK, and Readline (the three
+            GPLed optional library dependences for CVC4), and how to ensure you have a
+            build that doesn't link against GPLed libraries.""" ]
+        , p [] [ text """Redistribution and use in source and binary forms, with or without
+            modification, are permitted provided that the following conditions are
+            met:""" ]
+        , p [] [ text """1. Redistributions of source code must retain the above copyright
+            notice, this list of conditions and the following disclaimer.""" ]
+        , p [] [ text """2. Redistributions in binary form must reproduce the above copyright
+            notice, this list of conditions and the following disclaimer in the
+            documentation and/or other materials provided with the distribution.""" ]
+        , p [] [ text """3. Neither the name of the copyright holder nor the names of its
+            contributors may be used to endorse or promote products derived from
+            this software without specific prior written permission.""" ]
+        , p [] [ text """THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT OWNERS AND CONTRIBUTORS
+            ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+            LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+            A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+            OWNERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+            SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+            LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+            DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+            THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+            (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+            OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.""" ]
+        , h5 [] [ text "Spectre" ]
+        , p [] [ text "The MIT License (MIT)" ]
+        , p [] [ text "Copyright (c) 2016 - 2018 Yan Zhu" ]
+        , p [] [ text """Permission is hereby granted, free of charge, to any person obtaining a copy
+            of this software and associated documentation files (the "Software"), to deal
+            in the Software without restriction, including without limitation the rights
+            to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+            copies of the Software, and to permit persons to whom the Software is
+            furnished to do so, subject to the following conditions:""" ]
+        , p [] [ text """The above copyright notice and this permission notice shall be included in all
+            copies or substantial portions of the Software.""" ]
+        , p [] [ text """THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+            IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+            FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+            AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+            LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+            OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+            SOFTWARE.""" ]
+        ]
+
+
+copyrightNoticeView : List (Html Msg)
+copyrightNoticeView =
+    [ a [ class "btn btn-primary", href "#copyright-modal" ] [ text "copyright" ]
+    , div [ class "modal", id "copyright-modal" ]
+        [ a [ class "modal-overlay", href "#modals", ariaLabel "Close" ] []
+        , div [ class "modal-container", role "document" ]
+            [ div [ class "modal-header" ]
+                [ div [ class "modal-title h5" ] [ text "Copyright" ] ]
+            , div [ class "modal-body" ] [ copyrightView ]
+            , div [ class "modal-footer" ] []
+            ]
+        ]
+    ]
+
+
+headerView : Html Msg
+headerView =
+    header []
+        [ div [ class "columns" ]
+            [ div [ class "column col-auto" ] [ h1 [] [ text "Plover Garden" ] ]
+            , div [ class "column col-2 col-ml-auto" ] copyrightNoticeView
             ]
         ]
 
